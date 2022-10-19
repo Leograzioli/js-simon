5049825eb5360f7305acc0c04137cf5ddee30ba1
Leograzioli <leograzioli@gmail.com>
2022-10-19 14:37:40 +0200

    cambiato per arrow function

--- js/script.js
+++ js/script.js
@@ -3,24 +3,29 @@ const RndNumbersArray = [];
 const userNumbersArray = [];
 const guessedNumber = []
 
-
-
-
 while (RndNumbersArray.length < 5) {
     const element = getRndInteger(1,100);
     if (!RndNumbersArray.includes(element)){
         numbersContainer.innerHTML +=` ${element}` ;
         RndNumbersArray.push(element);
-    }
-    
+    } 
 }
 
+//timeout to hide the class
 setTimeout( () => {
     numbersContainer.classList.add("hidden");
 }, 3000);
 
-setTimeout(askUser, 3100);
+// to ask user the numbers 
+const askUser = () => {
+    for (let i = 0; i < 5; i++) {
+        let userImput = parseInt(prompt("indovina il numero"));        
+        userNumbersArray.push(userImput)
+    }
+    result();
+}
 
+setTimeout(askUser, 3100);
 
 //FUNCTIONS
 
@@ -30,17 +35,7 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-
-// to ask user the numbers 
-function askUser() {
-    for (let i = 0; i < 5; i++) {
-        let userImput = parseInt(prompt("indovina il numero"));        
-        userNumbersArray.push(userImput)
-    }
-    result();
-}
-
-
+//check if the RndNumbersArray includes the inputed number 
 function result() {
     for (let i = 0; i < RndNumbersArray.length; i++) {
         const elementI = RndNumbersArray[i];
@@ -51,6 +46,5 @@ function result() {
             }
         }
     }
-
     console.log(`quantita di numeri indovinati: ${guessedNumber.length}.  numeri indovinati: ${guessedNumber} `);
 }
