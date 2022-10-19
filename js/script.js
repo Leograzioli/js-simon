dfd19496c657004f429940a3f2c2e41093859542
Leograzioli <leograzioli@gmail.com>
2022-10-19 15:12:48 +0200

    spostato function dentro al timeout

--- js/script.js
+++ js/script.js
@@ -23,18 +23,6 @@ const result = () => {
     finalResult.innerHTML += (`quantita di numeri indovinati: ${guessedNumber.length}.  i numeri indovinati sono: ${guessedNumber} `);
 };
 
-// to ask user the numbers 
-const askUser = () => {
-    for (let i = 0; i < 5; i++) {
-        let userImput = parseInt(prompt("indovina il numero"));
-        if (isNaN(userImput)) {
-            userImput = parseInt(prompt("hai inserito una lettera, indovina il NUMERO"));
-        }      
-        userNumbersArray.push(userImput)
-    }
-    result();
-};
-
 //generate random number and add to RndNumbersArray without repeated number.
 while (RndNumbersArray.length < 5) {
     const element = getRndInteger(1,100);
@@ -45,13 +33,24 @@ while (RndNumbersArray.length < 5) {
     } 
 }
 
-//timeout to hide the class
+//timeout to hide the numbers in DOM
 setTimeout( () => {
     numbersContainer.classList.add("hidden");
 }, 3000);
 
-setTimeout(askUser, 3100);
+// to ask user the numbers 
+setTimeout( () => {
+    for (let i = 0; i < 5; i++) {
+        let userImput = parseInt(prompt("indovina il numero"));
+        if (isNaN(userImput)) {
+            userImput = parseInt(prompt("hai inserito una lettera, indovina il NUMERO"));
+        }      
+        userNumbersArray.push(userImput)
+    }
+    result();
+}, 3100);
 
+//to show the numbers in DOM
 setTimeout( () => {
     numbersContainer.classList.remove("hidden");
 }, 3200);
